1ea80de42c6fee1c7cdf000113c3d9ac64444521
Saksham27092006 <saksham.agrawal2025.glbajajgroup.org>
2026-07-06 11:21:52 +0530

    Day-16 assignment is uploaded

---
+++ Day-16/Question_62.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, j;
+    int maxFreq = 0, maxElement, count;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        count = 1;
+
+        for(j = 0; j < i; j++)
+        {
+            if(arr[i] == arr[j])
+                break;
+        }
+
+        if(j != i)
+            continue;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[i] == arr[j])
+                count++;
+        }
+
+        if(count > maxFreq)
+        {
+            maxFreq = count;
+            maxElement = arr[i];
+        }
+    }
+
+    printf("Element with maximum frequency = %d\n", maxElement);
+    printf("Frequency = %d\n", maxFreq);
+
+    return 0;
+}
